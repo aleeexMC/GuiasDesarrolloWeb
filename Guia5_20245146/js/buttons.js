fb20f9eb71cf2bb131e9786827a8612c46706378
aleeexMC <alexchamul.mc2@gmail.com>
2025-11-03 18:39:25 -0600

    actualizacion guia 5

--- Guia5_20245146/js/buttons.js
+++ Guia5_20245146/js/buttons.js
@@ -10,6 +10,16 @@ function confirmacion() {
 function capturarDatos () {
     let nombre = prompt("¿Cuál es su nombre?");
     let edad = prompt("¿Cuál es su edad?", 0);
+
     alert(`Su nombre es ${nombre} y su edad ${edad}`);
+}
+
+function dibujarParrafo() {
+    let parrafo = prompt(
+        "Escriba la información que desea visualizar en el párrafo"
+    );
+
+    const p = document.querySelector("#idParrafo");
+    p.innerHTML = parrafo;
 }
 
---
+++ Guia6_20245146/js/registroAcademico.js
@@ -0,0 +1,70 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const containerEstudiantes = document.querySelector(
+        "#idContainerEstudiantes"
+    );
+
+    const btnAddEstudiante = document.querySelector("#idBtnAgregarEstudiante");
+    const btnViewEstudiantes = document.querySelector("#idBtnMostrarEstudiantes");
+
+    btnAddEstudiante.addEventListener("click", addEstudiantes);
+    btnViewEstudiantes/this.addEventListener("click", viewEstudiantes);
+
+    let arrayEstudiantes = new Array();
+
+    function addEstudiantes() {
+        const inputCarnet = document
+            .querySelector("#inputCarnet")
+            .value.toString()
+            .toUpperCase();
+        const inputNombre = document
+            .querySelector("#inputNombre")
+            .value.toString()
+            .toUpperCase();
+        const inputApellidos = document
+            .querySelector("#inputApellidos")
+            .value.toString()
+            .toUpperCase();
+
+        if (inputCarnet != "" && inputNombre != "" && inputApellidos != "") {
+            arrayEstudiantes.push(
+                new Array(inputCarnet, inputNombre, inputApellidos)
+            );
+            alert("Se registró el nuevo estudiante");
+
+            document.querySelector("#inputCarnet").value = "";
+            document.querySelector("#inputNombre").value = "";
+            document.querySelector("#inputApellidos").value = "";
+            document.querySelector("#inputCarnet").focus()
+        } else {
+            alert("Faltan campos que completar");
+        }
+    }
+
+    function viewEstudiantes() {
+        let totalEstudiantes = arrayEstudiantes.length;
+        if (totalEstudiantes > 0) {
+            let carnet;
+            let nombres;
+            let apellidos;
+            let table = "<table class='table table-light table-striped'>";
+            table += "<thead>";
+            table += "<tr>";
+            table += "<th scope='col' style='width: 5%;'>#</th>";
+            table += "<th scope='col' style='width: 15%;'>Carnet</th>";
+            table += "<th scope='col'>Nombres</th>";
+            table += "<th scope='col'>Apellidos</th>";
+            table += "</tr>";
+            table += "</thead>";
+            table += "<tbody>";
+
+            for (let i = 0; i < arrayEstudiantes.length; i++) {
+                carnet = arrayEstudiantes[i][0];
+                nombres = arrayEstudiantes[i][1];
+                apellidos = arrayEstudiantes[i][2];
+
+                
+            }
+        }
+    }
+}
+)
